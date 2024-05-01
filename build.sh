a1d79b3b3e676617618b857a8c8b7755be58d6bd
TohurTV <joshwebb84@outlook.com>
2024-04-30 23:47:07 -0600

    remove kde-connect

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 rpm-ostree override remove pipewire-pulseaudio --install pulseaudio
 rpm-ostree override remove ptyxis
+rpm-ostree override remove kde-connect-libs kdeconnectd kde-connect
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
