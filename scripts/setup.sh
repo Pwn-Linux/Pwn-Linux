8e5e47216988b4829054d2e3314a81166ece71c6
TohurTV <joshwebb84@outlook.com>
2024-05-07 22:57:26 -0600

    merging all image builds into one Containerfile

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 set -ouex pipefail
 
+mkdir -p /var/lib/alternatives && \
 curl -Lo /etc/yum.repos.d/tohur-Pwn-fedora-40.repo https://copr.fedorainfracloud.org/coprs/tohur/Pwn/repo/fedora-40/tohur-Pwn-fedora-40.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/tohur-Pwn-fedora-40.repo && \
 ostree container commit
