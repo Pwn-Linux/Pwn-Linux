6d1291aab469c22eb52defb4872fcbb4720e880e
TohurTV <joshwebb84@outlook.com>
2024-06-23 21:13:47 -0600

    adding new packages for our copr

--- scripts/kinoite/prepare.sh
+++ scripts/kinoite/prepare.sh
@@ -4,8 +4,6 @@ set -ouex pipefail
 
 curl -Lo /etc/yum.repos.d/tohur-Pwn-fedora-40.repo https://copr.fedorainfracloud.org/coprs/tohur/Pwn/repo/fedora-40/tohur-Pwn-fedora-40.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/tohur-Pwn-fedora-40.repo && \
-curl -Lo /etc/yum.repos.d/hazel-bunny-ricing-fedora-40.repo https://copr.fedorainfracloud.org/coprs/hazel-bunny/ricing/repo/fedora-40/hazel-bunny-ricing-fedora-40.repo && \
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/hazel-bunny-ricing-fedora-40.repo && \
 curl -Lo /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/mullvad.repo && \
 mkdir -p /var/lib/alternatives && \
