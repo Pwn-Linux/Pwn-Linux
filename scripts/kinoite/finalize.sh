86921504b1ffcdd40792aa20efc4307d1ffd87fb
TohurTV <joshwebb84@outlook.com>
2024-06-25 02:28:09 -0600

    reverting mount binding to /mnt

--- scripts/kinoite/finalize.sh
+++ scripts/kinoite/finalize.sh
@@ -81,15 +81,5 @@ echo 'flatpak run --branch=stable --arch=x86_64 --command=konsole org.kde.konsol
 chmod +x /usr/bin/konsole && \
 ostree container commit
 
-#setup bind mount of /var/mnt to /mnt
-rm /mnt && \
-systemctl enable mnt.mount && \
-ostree container commit
-
-#setup bind mount of /var/mnt to /mnt
-rm /home && \
-systemctl enable home-mnt.mount && \
-ostree container commit
-
 #remove build scripts
 rm -vr /scripts
